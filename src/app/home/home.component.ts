cd0c3f1f343329eea3361cceb28ba1d1d522a844
Ant <hark777@hotmail.com>
2023-07-15 15:48:31 +1000

    add-data-to-component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IHousingLocation } from '../interfaces/ihousing-location';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: IHousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 
 }
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { IHousingLocation } from '../interfaces/ihousing-location';
+
+@Component({
+  selector: 'app-housing-location',
+  templateUrl: './housing-location.component.html',
+  styleUrls: ['./housing-location.component.less']
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: IHousingLocation;
+}
